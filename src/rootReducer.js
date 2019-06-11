1360de2253843b603d58ff89c866a3465360b292
Deepali2 <hdgarg@gmail.com>
2019-06-10 21:50:10 -0700

    refactors the reducers so it can implement the action creators  Please enter the commit message for your changes. Lines starting

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,19 +1,17 @@
-import {ADD_TODO, REMOVE_TODO, UPDATE_TODO} from "./constants";
+import {ADD_TODO, REMOVE_TODO, GET_TODOS} from "./constants";
 
 const initialState = {
   todos: [],
-  id: 0
 }
 
 export default function rootReducer(state=initialState, action) {
   switch (action.type) {
+    case GET_TODOS:
+      return {...state, todos:action.data}
     case ADD_TODO:
-      var newState = {...state};
-      newState.id++;
-      let todos = [...newState.todos, {todo:action.todo, id: newState.id}];
-      return {...newState, todos};
+      return {...state, todos: [...state.todos, action.todo]};
     case REMOVE_TODO:
-       let newTodos = state.todos.filter(todo => todo.id !== action.id);
+       let newTodos = state.todos.filter(todo => todo._id !== action.id);
        return {...state, todos: newTodos};
     default:
       return state;
